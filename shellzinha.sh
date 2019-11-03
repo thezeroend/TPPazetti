82dd48b44e7104402582e28316e699d41b41c671
Matheus Soares <45151533+thezeroend@users.noreply.github.com>
2019-11-02 22:00:14 -0300

    Ultima versão

--- shellzinha.sh
+++ shellzinha.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #Salva a variavel em forma global
+#Para funcionar em qualquer função
 barra="+------------------------------------------------+"
 
 #Função Principal
@@ -16,6 +17,7 @@ echo "|  3 - Novo arq com Nomes em letra maiuscula     |"
 echo "|  4 - Novo arq sem cidades repetidas            |"
 echo "|  5 - Novo arq com nome e cidade                |"
 echo "|  6 - Outros                                    |"
+echo "|  7 - Sair do programa                          |"
 echo $barra
 echo -n "Entre com uma das opções acima: " ; read cod_ini
 # -n parametro para permitir inserir o read na mesma linha
@@ -30,11 +32,13 @@ case $cod_ini in
 	4)escolha4 ;;
 	5)escolha5 ;;
 	6)escolha6 ;;
+	7)exit;;
 	*)cod_ini=0;;
 esac
 
 #Se o cod_ini for = 0
-#Vai ficar em loop aguardando uma resposta correta 
+#Vai entrar no if e retornar para o inicio
+#até obter uma resposta diferente 
 if [ $cod_ini -lt 1 ]
 then
 	#limpa a tela
@@ -47,7 +51,7 @@ then
 	echo ""
 
 	#Entra em modo de espera por 2 segundos
-	sleep 1
+	sleep 2
 
 	#Retorna para o começo da função
 	#Para entrar com o valor correto
@@ -55,6 +59,7 @@ then
 fi
 }
 
+#Função escolha1
 escolha1(){
 	#Chama a função para criar arquivo
 	cria_arq
@@ -68,6 +73,7 @@ escolha1(){
 	final
 }	
 
+#Função escolha2
 escolha2(){
 	#Chama a função para criar arquivo
 	cria_arq
@@ -79,6 +85,7 @@ escolha2(){
 	final
 }	
 
+#Função escolha3
 escolha3(){
 	#Chama a função para criar arquivo
 	cria_arq
@@ -90,6 +97,7 @@ escolha3(){
 	final
 }
 
+#Função escolha4
 escolha4(){
 	#Chama a função para criar arquivo
 	cria_arq
@@ -103,6 +111,7 @@ escolha4(){
 	
 }
 
+#Função escolha5
 escolha5(){
 	#Chama a função para criar arquivo
 	cria_arq
@@ -114,6 +123,113 @@ escolha5(){
 	final
 }
 
+#Função escolha6
+escolha6(){
+	#limpa a tela
+	clear
+
+	#Exibe o Menu
+	echo $barra
+	echo "|    Selecione a opção que deseja executar       |"
+	echo $barra
+	echo "|  1 - Visualizar o arquivo 1                    |"
+	echo "|  2 - Visualizar o arquivo 2                    |"
+	echo "|  3 - Visualizar o arquivo 3                    |"
+	echo "|  4 - Voltar ao menu principal                  |"
+	echo $barra
+	echo -n "Entre com uma das opções acima: " ; read cod_menu
+	# -n parametro para permitir inserir o read na mesma linha
+	# read le o que o usuário digitar e salva na variavel cod_ini
+
+	#Caso o Codigo seja um dos escolhidos
+	#Leva para respectiva função
+	case $cod_menu in 
+		1)cod_menu=1 lerarq ;;
+		2)cod_menu=2 lerarq ;;
+		3)cod_menu=3 lerarq ;;
+		4)principal ;;
+		*)cod_menu=0;;
+	esac
+
+	#Se o cod_ini for = 0
+	#Vai ficar em loop aguardando uma resposta correta 
+	if [ $cod_menu -lt 1 ]
+	then
+		#limpa a tela
+		clear
+
+		#Exibe mensagem
+		echo $barra
+		echo "|    INSIRA UM VALOR VALIDO PARA CONTINUAR       |"
+		echo $barra
+		echo ""
+
+		#Entra em modo de espera por 2 segundos
+		sleep 1
+
+		#Retorna para o começo da função
+		#Para entrar com o valor correto
+		escolha6
+	fi
+}
+
+#Função lerarq
+lerarq(){
+	#Concatena o arquivo escolhido a palavra arq
+	#Formando assim o nome do arquivo
+	#Ex.: arq1, arq2, arq3
+	ler_arq="arq$cod_menu"
+
+	#Limpa a tela
+	clear
+
+	#Exibe o Menu
+	echo $barra
+	echo "|  Visualizando os dados do arquivo arq$cod_menu         |"
+	echo $barra
+	#As aspas le o conteudo dentro da variavel $ler_arq
+	#E o cat imprime na tela o conteúdo do arquivo
+	cat "$ler_arq"
+	echo $barra
+	echo ""
+	echo "+-----------------------------------------------------------+"
+	echo "| Deseja verificar outro arquivo? (S)Sim (N)Não             |" 
+	echo "+-----------------------------------------------------------+"; read decisao
+
+	#Deixa a primeira letra maiuscula
+	decisao=${decisao^}
+
+	#Verifica o resultado da variavel decisao
+	case $decisao in
+		S) $decisao = "S" ;;
+		N) $decisao = "N" ;;
+		*) $decisao = "Opcao Invalida!" ;;
+	esac
+
+	#Se for "S"
+	if [ $decisao == "S" ];
+	then
+		#Limpa a tela
+		clear
+		#Volta para o começo
+		escolha6
+	elif [ $decisao == "N" ] #Se For "N"
+	then
+		#Limpa a tela
+		clear
+		#Sai do aplicativo
+		principal
+	else #Se não for nenhum dos dois acima retorna ao menu principal
+		echo "Erro ao continuar, você foi redirecionado para o menu principal'." 
+    	#Tempo de espera 2 segundos
+    	sleep 2
+    	#Volta pro começo da função final
+    	principal
+	fi
+	
+}
+
+#Função final
 final(){
 	#Limpa a tela
 	clear
